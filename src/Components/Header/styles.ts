5c60da960150a611478d2890ead141e5828a3bc3
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2024-11-11 08:38:42 -0300

    feat(Layout): adicionando component Header no layout

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -3,24 +3,27 @@ import styled from 'styled-components'
 export const Hero = styled.header`
   display: block;
   width: 100%;
-  height: 364px;
+  // height: 364px;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
 `
 
 export const Container = styled.div`
-  margin: 0 auto;
+  height: 364px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   padding-top: 64px;
 
   img {
     display: block;
     margin: 0 auto;
     width: 125px;
-    padding-bottom: 140px;
   }
 
   h1 {
+    padding-bottom: 40px;
     margin: 0 auto;
     max-width: 540px;
     font-weight: 900;
