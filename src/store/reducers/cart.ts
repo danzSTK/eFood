b91f0d25bb29c7e1fd4fe347a5cc36e15a440297
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2024-12-06 13:57:39 -0300

    configuraçao inicial do cartSlice

---
+++ src/store/reducers/cart.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Cardapio } from '../../pages/Home'
+
+type CartState = {
+  items: Cardapio[]
+}
+
+const initialState: CartState = {
+  items: []
+}
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    add: (state, action: PayloadAction<Cardapio>) => {
+      const produto = state.items.find((item) => item.id === action.payload.id)
+
+      if (!produto) {
+        state.items.push(action.payload)
+      } else alert('Esse produto já está no carrinho')
+    },
+    remove: (state, aciton: PayloadAction<number>) => {
+      state.items = state.items.filter((item) => item.id !== aciton.payload)
+    }
+  }
+})
